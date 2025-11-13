75e30b709c2111c68c353394520344fb5dce6e2a
wonza-hub <wonza8585@gmail.com>
2025-11-13 15:38:18 +0900

    design: 전역 레이아웃 추가

--- src/pages/sign-up-page.tsx
+++ src/pages/sign-up-page.tsx
@@ -1,3 +1,3 @@
 export default function SignUpPage() {
-  return <div></div>;
+  return <div>SignUp Page</div>;
 }
--- src/root-route.tsx
+++ src/root-route.tsx
@@ -6,20 +6,23 @@ import IndexPage from "./pages/index-page";
 import PostDetailPage from "./pages/post-detail-page";
 import ProfileDetailPage from "./pages/profile-detail-page";
 import ResetPasswordPage from "./pages/reset-password-page";
+import GlobalLayout from "./components/layout/global-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/forget-password" element={<ForgetPasswordPage />} />
+      <Route element={<GlobalLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
 
-      <Route path="/" element={<IndexPage />} />
-      <Route path="/post/:postId" element={<PostDetailPage />} />
-      <Route path="/profile/:userId" element={<ProfileDetailPage />} />
-      <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-      <Route path="/*" element={<Navigate to={"/"} />} />
+        <Route path="/*" element={<Navigate to={"/"} />} />
+      </Route>
     </Routes>
   );
 }
