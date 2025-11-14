c2f8cb2bab12d9f0fb956ee89c9510529cf62c51
wonza-hub <wonza8585@gmail.com>
2025-11-14 15:51:50 +0900

    feat: 깃허브 로그인 기능 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,5 @@
 import supabase from "@/lib/supabase";
+import type { Provider } from "@supabase/supabase-js";
 
 export async function signUp({
   email,
@@ -27,6 +28,19 @@ export async function signInWithPassword({
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
+/**
+ * OAuth 로그인
+ */
+export async function signInWithOAuth(provider: Provider) {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider,
   });
 
   if (error) throw error;
--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -3,12 +3,15 @@ import { Input } from "@/components/ui/input";
 import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
 import { useState } from "react";
 import { Link } from "react-router";
+import gitHubLogo from "@/assets/github-mark.svg";
+import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithOAuth } = useSignInWithOAuth();
 
   const handleSignInWithPasswordClick = () => {
     if (email.trim() === "") return;
@@ -18,6 +21,10 @@ export default function SignInPage() {
       email,
       password,
     });
+  };
+
+  const handleSignInWithGitHubClick = () => {
+    signInWithOAuth("github");
   };
 
   return (
@@ -39,9 +46,17 @@ export default function SignInPage() {
           placeholder="password"
         />
       </div>
-      <div>
+      <div className="flex flex-col gap-2">
         <Button onClick={handleSignInWithPasswordClick} className="w-full">
           로그인
+        </Button>
+        <Button
+          onClick={handleSignInWithGitHubClick}
+          className="w-full"
+          variant={"outline"}
+        >
+          <img src={gitHubLogo} className="h-4 w-4" />
+          GitHub 계정으로 로그인
         </Button>
       </div>
       <div>
