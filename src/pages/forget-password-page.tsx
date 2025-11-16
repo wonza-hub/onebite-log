6065d3ccbdcaae57233b220a224d9f129a0cbba2
wonza-hub <wonza8585@gmail.com>
2025-11-16 19:57:32 +0900

    feat: 비밀번호 재설정 위한 기존 정보 입력 페이지 추가

--- src/pages/forget-password-page.tsx
+++ src/pages/forget-password-page.tsx
@@ -1,3 +1,63 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRequestPasswordResetEmail } from "@/hooks/mutations/use-request-password-reset-email";
+import { generateAuthErrorMessage } from "@/lib/error";
+import { useState } from "react";
+import { toast } from "sonner";
+
+/**
+ * PAGE: 비밀번호 재설정 위한 기존 정보 입력
+ * @returns
+ */
 export default function ForgetPasswordPage() {
-  return <div></div>;
+  const [email, setEmail] = useState("");
+
+  const {
+    mutate: requestPasswordResetEmail,
+    isPending: isRequestPasswordResetEmailPending,
+  } = useRequestPasswordResetEmail({
+    onSuccess: () => {
+      toast.info("인증 메일이 성공적으로 발송되었습니다.", {
+        position: "top-center",
+      });
+      setEmail("");
+    },
+    onError: (error) => {
+      const message = generateAuthErrorMessage(error);
+      toast.error(message, {
+        position: "top-center",
+      });
+      setEmail("");
+    },
+  });
+
+  const handleSendEmailClick = () => {
+    if (email.trim() === "") return;
+    requestPasswordResetEmail(email);
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-xl font-bold">비밀번호를 잊으셨나요?</div>
+        <div className="text-muted-foreground">
+          이메일로 비밀번호를 재설정 할 수 있는 인증 링크를 보내드립니다.
+        </div>
+      </div>
+      <Input
+        disabled={isRequestPasswordResetEmailPending}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="py-6"
+        placeholder="example@abc.com"
+      />
+      <Button
+        disabled={isRequestPasswordResetEmailPending}
+        onClick={handleSendEmailClick}
+        className="w-full"
+      >
+        인증 메일 요청하기
+      </Button>
+    </div>
+  );
 }
