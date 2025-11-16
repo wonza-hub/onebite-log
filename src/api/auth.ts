2e9a44c033def16889fc40e3104243db77455527
wonza-hub <wonza8585@gmail.com>
2025-11-16 19:54:49 +0900

    feat: 비밀번호 재설정 이메일 요청, 완료 요청 api 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -47,3 +47,23 @@ export async function signInWithOAuth(provider: Provider) {
 
   return data;
 }
+
+export async function requestPasswordResetEmail(email: string) {
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `${import.meta.env.VITE_PUBLIC_URL}/reset-password`,
+  });
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function updatePassword(password: string) {
+  const { data, error } = await supabase.auth.updateUser({
+    password,
+  });
+
+  if (error) throw error;
+
+  return data;
+}
