f08ad2e8c0c72c3cc96f9dfaa377749e4e0962d2
wonza-hub <wonza8585@gmail.com>
2025-11-16 19:53:41 +0900

    feat: mutation 커스텀훅 콜백 파라미터 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
 import type { Database } from "@/database.types";
 
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
+
+export type TUseMutationCallback = {
+  onMutate?: () => void;
+  onSuccess?: () => void;
+  onError?: (error: Error) => void;
+  onSettled?: () => void;
+};
