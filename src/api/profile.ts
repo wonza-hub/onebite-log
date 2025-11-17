80593051b7d47e07ebce3742b43a0326ac162877
wonza-hub <wonza8585@gmail.com>
2025-11-17 21:51:18 +0900

    feat: 프로필 생성 api 추가

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,10 +1,34 @@
 import supabase from "@/lib/supabase";
+import { getRandomNickname } from "@/lib/utils";
 
+/**
+ * READ: 프로필
+ * @param userId
+ */
 export async function fetchProfile(userId: string) {
   const { data, error } = await supabase
     .from("profile")
     .select("*")
     .eq("id", userId)
+    .single();
+
+  if (error) throw error;
+
+  return data;
+}
+
+/**
+ * CREATE: 프로필
+ * @param userId
+ */
+export async function createProfile(userId: string) {
+  const { data, error } = await supabase
+    .from("profile")
+    .insert({
+      id: userId,
+      nickname: getRandomNickname(),
+    })
+    .select()
     .single();
 
   if (error) throw error;
