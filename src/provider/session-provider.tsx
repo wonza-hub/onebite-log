4b8801b7811a392a07496ee9b9bf7bd0083fc962
wonza-hub <wonza8585@gmail.com>
2025-11-17 23:43:36 +0900

    feat: 세션 provider에서 프로필 조회하는 기능 추가

--- src/provider/session-provider.tsx
+++ src/provider/session-provider.tsx
@@ -1,11 +1,17 @@
 import GlobalLoader from "@/components/global-loader";
+import { useProfileData } from "@/hooks/queries/use-profile-data";
 import supabase from "@/lib/supabase";
-import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useIsSessionLoaded, useSession, useSetSession } from "@/store/session";
 import { useEffect, type ReactNode } from "react";
 
 export default function SessionProvider({ children }: { children: ReactNode }) {
+  const session = useSession();
   const setSession = useSetSession();
   const isSessionLoaded = useIsSessionLoaded();
+
+  const { data: profile, isLoading: isProfileLoading } = useProfileData(
+    session?.user.id,
+  );
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -13,7 +19,11 @@ export default function SessionProvider({ children }: { children: ReactNode }) {
     });
   }, []);
 
+  // onAuthStateChange 결과 세션이 있는 경우
+  // 프로필 정보 가져오는 중인 경우
+  // 로딩 UI 표시
   if (!isSessionLoaded) return <GlobalLoader />;
+  if (isProfileLoading) return <GlobalLoader />;
 
   return children;
 }
