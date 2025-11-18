a1cb56a17773b1b9065e0f75b080b233c658a71f
wonza-hub <wonza8585@gmail.com>
2025-11-19 00:18:21 +0900

    feat: 이미지 포함한 포스트 생성 과정 중 에러 발생 시 해당 포스트 삭제 처리 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -15,6 +15,18 @@ export async function createPost(content: string) {
   return data;
 }
 
+export async function deletePost(id: number) {
+  const { data, error } = await supabase
+    .from("post")
+    .delete()
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
 export async function updatePost(post: Partial<PostEntity> & { id: number }) {
   const { data, error } = await supabase
     .from("post")
@@ -40,25 +52,32 @@ export async function createPostWithImages({
   const post = await createPost(content);
   if (images.length === 0) return post;
 
-  // 이미지 업로드 후 반환값으로 public url 얻음
-  const imageUrls = await Promise.all(
-    images.map((image) => {
-      const fileExt = image.name.split(".").pop() || "webp";
-      const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExt}`;
-      const filePath = `${userId}/${post.id}/${fileName}`;
+  // 이미지도 첨부한 경우 처리
+  try {
+    // 이미지 업로드 후 반환값으로 public url 얻음
+    const imageUrls = await Promise.all(
+      images.map((image) => {
+        const fileExt = image.name.split(".").pop() || "webp";
+        const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExt}`;
+        const filePath = `${userId}/${post.id}/${fileName}`;
 
-      return uploadImage({
-        file: image,
-        filePath,
-      });
-    }),
-  );
+        return uploadImage({
+          file: image,
+          filePath,
+        });
+      }),
+    );
 
-  // 포스트 테이블 갱신
-  const updatedPost = await updatePost({
-    id: post.id,
-    image_urls: imageUrls,
-  });
+    // 포스트 테이블 갱신
+    const updatedPost = await updatePost({
+      id: post.id,
+      image_urls: imageUrls,
+    });
 
-  return updatedPost;
+    return updatedPost;
+  } catch (error) {
+    // 이미지 업로드 중 에러 발생 시 해당 포스트 삭제 처리
+    await deletePost(post.id);
+    throw error;
+  }
 }
