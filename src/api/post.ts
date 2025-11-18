85c5c06c1132792f593652749390913e6dccc8cb
wonza-hub <wonza8585@gmail.com>
2025-11-19 00:10:50 +0900

    feat: 이미지 포함한 포스트 생성 api 추가

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,10 +1,64 @@
 import supabase from "@/lib/supabase";
+import { uploadImage } from "./image";
+import type { PostEntity } from "@/types";
 
 export async function createPost(content: string) {
-  const { data, error } = await supabase.from("post").insert({
-    content,
-  });
+  const { data, error } = await supabase
+    .from("post")
+    .insert({
+      content,
+    })
+    .select()
+    .single();
 
   if (error) throw error;
   return data;
+}
+
+export async function updatePost(post: Partial<PostEntity> & { id: number }) {
+  const { data, error } = await supabase
+    .from("post")
+    .update(post)
+    .eq("id", post.id)
+    .select()
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export async function createPostWithImages({
+  content,
+  images,
+  userId,
+}: {
+  content: string;
+  images: File[];
+  userId: string;
+}) {
+  // 새로운 포스트 생성
+  const post = await createPost(content);
+  if (images.length === 0) return post;
+
+  // 이미지 업로드 후 반환값으로 public url 얻음
+  const imageUrls = await Promise.all(
+    images.map((image) => {
+      const fileExt = image.name.split(".").pop() || "webp";
+      const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExt}`;
+      const filePath = `${userId}/${post.id}/${fileName}`;
+
+      return uploadImage({
+        file: image,
+        filePath,
+      });
+    }),
+  );
+
+  // 포스트 테이블 갱신
+  const updatedPost = await updatePost({
+    id: post.id,
+    image_urls: imageUrls,
+  });
+
+  return updatedPost;
 }
