a8c1f623b6308e6ea79a74126dfb8639afe4957a
wonza-hub <wonza8585@gmail.com>
2025-11-20 03:38:24 +0900

    feat: 전역 얼럿 모달 컴포넌트 추가

---
+++ src/components/modal/alert-modal.tsx
@@ -0,0 +1,47 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useAlertModal } from "@/store/alert-modal";
+
+/**
+ * COMPONENT: 얼럿 모달
+ */
+export default function AlertModal() {
+  const store = useAlertModal();
+  if (!store.isOpen) return null;
+
+  const handleCancelClick = () => {
+    if (store.onNegative) store.onNegative();
+    store.actions.close();
+  };
+  const handleActionClick = () => {
+    if (store.onPositive) store.onPositive();
+    store.actions.close();
+  };
+
+  return (
+    <AlertDialog open={store.isOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{store.title}</AlertDialogTitle>
+          <AlertDialogDescription>{store.description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={handleCancelClick}>
+            취소
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleActionClick}>
+            확인
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
