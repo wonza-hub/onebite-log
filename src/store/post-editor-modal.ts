b1f54b26b9c7a654c773b1b7065ff35598ad3a44
wonza-hub <wonza8585@gmail.com>
2025-11-24 18:33:17 +0900

    feat: 모달 전역 상태 스토어의 생성/수정에 따라 열기 타입 및 액션 분리

--- src/store/post-editor-modal.ts
+++ src/store/post-editor-modal.ts
@@ -4,15 +4,38 @@ import { combine, devtools } from "zustand/middleware";
 /**
  * STORE: 포스트 작성 모달 전역 상태
  */
+type TCreateMode = {
+  isOpen: true;
+  type: "CREATE";
+};
+type TEditMode = {
+  isOpen: true;
+  type: "EDIT";
+  postId: number;
+  content: string;
+  imageUrls: string[] | null;
+};
+
+type TOpenState = TCreateMode | TEditMode;
+type TCloseState = {
+  isOpen: false;
+};
+
+type TState = TCloseState | TOpenState;
 const initialState = {
   isOpen: false,
-};
+} as TState;
 const usePostEditorModalStore = create(
   devtools(
     combine(initialState, (set) => ({
       actions: {
-        open: () => {
-          set({ isOpen: true });
+        // 포스트 생성 시
+        openCreate: () => {
+          set({ isOpen: true, type: "CREATE" });
+        },
+        // 포스트 수정 시
+        openEdit: (param: Omit<TEditMode, "isOpen" | "type">) => {
+          set({ isOpen: true, type: "EDIT", ...param });
         },
         close: () => {
           set({ isOpen: false });
@@ -23,14 +46,21 @@ const usePostEditorModalStore = create(
   ),
 );
 
-export const useOpenPostEditorModal = () => {
-  return usePostEditorModalStore((store) => store.actions.open);
+export const useOpenCreatePostModal = () => {
+  const openCreate = usePostEditorModalStore(
+    (store) => store.actions.openCreate,
+  );
+  return openCreate;
+};
+
+export const useOpenEditPostModal = () => {
+  const openEdit = usePostEditorModalStore((store) => store.actions.openEdit);
+  return openEdit;
 };
 
 export const usePostEditorModal = () => {
-  const {
-    isOpen,
-    actions: { open, close },
-  } = usePostEditorModalStore();
-  return { isOpen, open, close };
+  const store = usePostEditorModalStore();
+  // return store
+  // cf) 유니온 타입 TState에 대한 타입스크립트의 잘못된 추론을 방지
+  return store as typeof store & TState;
 };
