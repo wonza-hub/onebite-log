3458ac325b08699fc3a53fe0b9892749649dcf16
wonza-hub <wonza8585@gmail.com>
2025-11-26 18:52:12 +0900

    feat: 포스트 수정 후 캐시 수정 작업 추가

--- src/hooks/mutations/post/use-update-post.ts
+++ src/hooks/mutations/post/use-update-post.ts
@@ -1,12 +1,34 @@
 import { updatePost } from "@/api/post";
-import type { TUseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { TPost, TUseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
+/**
+ * HOOK: 포스트 수정
+ */
 export function useUpdatePost(callbacks?: TUseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: updatePost,
-    onSuccess: () => {
+    onSuccess: (updatedPost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<TPost>(
+        QUERY_KEYS.post.byId(updatedPost.id),
+        (prevPost) => {
+          if (!prevPost)
+            throw new Error(
+              `${updatedPost.id}에 해당하는 포스트를 캐시 데이터에서 찾을 수 없습니다.`,
+            );
+
+          // 스프레드 연산자는 뒷 내용이 앞 내용을 덮어씀에 유의
+          return {
+            ...prevPost,
+            ...updatedPost,
+          };
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
