16df41fc9698654c1fc101b0ceec4f5830e5b4a9
wonza-hub <wonza8585@gmail.com>
2025-11-26 18:50:17 +0900

    feat: 포스트 삭제 후 캐시 초기화 작업 추가

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,19 +1,23 @@
 import { deleteImagesInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { TUseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: TUseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletedPost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
 
       // 포스트 삭제 후 포스트에 연결된 스토리지 내 이미지들도 삭제
-      if (!deletedPost.image_urls || deletedPost.image_urls.length === 0)
-        return;
+      if (deletedPost.image_urls && deletedPost.image_urls.length > 0) {
+        await deleteImagesInPath(`${deletedPost.author_id}/${deletedPost.id}`);
+      }
 
-      await deleteImagesInPath(`${deletedPost.author_id}/${deletedPost.id}`);
+      queryClient.resetQueries({ queryKey: QUERY_KEYS.post.list });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
