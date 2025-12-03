fcc8e2ba7bec7a6e70586301c91c5ccc34a0949f
wonza-hub <wonza8585@gmail.com>
2025-12-03 17:46:48 +0900

    feat: 포스트 페칭 훅에 좋아요 여부 적용한 api 적용

--- src/hooks/queries/use-infinite-posts-data.ts
+++ src/hooks/queries/use-infinite-posts-data.ts
@@ -1,17 +1,19 @@
 import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/lib/constants";
 import { fetchPosts } from "@/api/post";
+import { useSession } from "@/store/session";
 
 const PAGE_SIZE = 5;
 export function useInfinitePostsData() {
   const queryClient = useQueryClient();
+  const session = useSession();
 
   return useInfiniteQuery({
     queryKey: QUERY_KEYS.post.list,
     queryFn: async ({ pageParam }) => {
       const from = pageParam * PAGE_SIZE;
       const to = from + PAGE_SIZE - 1;
-      const posts = await fetchPosts({ from, to });
+      const posts = await fetchPosts({ from, to, userId: session!.user.id });
       posts.forEach((post) => {
         queryClient.setQueryData(QUERY_KEYS.post.byId(post.id), post);
       });
--- src/hooks/queries/use-post-by-id-data.ts
+++ src/hooks/queries/use-post-by-id-data.ts
@@ -1,5 +1,6 @@
 import { fetchPostById } from "@/api/post";
 import { QUERY_KEYS } from "@/lib/constants";
+import { useSession } from "@/store/session";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function usePostByIdData({
@@ -10,9 +11,11 @@ export function usePostByIdData({
   type: "FEED" | "DETAIL";
 }) {
   const queryClient = useQueryClient();
+  const session = useSession();
+
   return useQuery({
     queryKey: QUERY_KEYS.post.byId(postId),
-    queryFn: () => fetchPostById(postId),
+    queryFn: () => fetchPostById({ postId, userId: session!.user.id }),
     // enabled: false,
     // enabled를 무조건 false로 설정하면 쿼리가 실행되지 않음
     // type에 따라 쿼리를 실행할 수 있도록 설정
