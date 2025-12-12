a34f57727a37c8eccf9329e25cdfd06c1f36075d
wonza-hub <wonza8585@gmail.com>
2025-12-13 00:10:26 +0900

    feat: supabase에 댓글 테이블 추가 및 프로젝트에 댓글 타입 반영

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import type { Database } from "@/database.types";
 
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
+export type CommentEntity = Database["public"]["Tables"]["comment"]["Row"];
 
 export type TPost = PostEntity & { author: ProfileEntity; isLiked: boolean };
 
